17c08f4267551ee08f05fc12b26ece7c3996721b
Meghabh <meghahavaragi@gmail.com>
2017-06-05 23:56:36 +0530

    executed the second testcase shouldTurnNorthFromWestForInputR()

--- src/Test/MarsRoverTest.java
+++ src/Test/MarsRoverTest.java
@@ -6,9 +6,15 @@ import static junit.framework.TestCase.assertEquals;
 public class MarsRoverTest {
     @Test
     public void shouldTurnWestFromNorthForInputL(){
-        MarsRover marsRover= new MarsRover(Directions.SOUTH);
+        MarsRover marsRover= new MarsRover(0,0,Directions.NORTH);
         marsRover.setDirections(Directions.Rotation.L);
-        assertEquals(Directions.EAST,marsRover.getDirection());
+        assertEquals(Directions.WEST,marsRover.getDirection());
+    }
+    @Test
+    public void shouldTurnNorthFromWestForInputR(){
+        MarsRover marsRover= new MarsRover(0, 0, Directions.WEST);
+        marsRover.setDirections(Directions.Rotation.R);
+        assertEquals(Directions.NORTH,marsRover.getDirection());
     }
 
 
