0ac4129e8265ece19c8e51c51e842f900841a637
Meghabh <meghahavaragi@gmail.com>
2017-06-06 00:16:23 +0530

    wrote the testcase for shouldBeInTheSameDirectionForCommandM() --

--- src/Test/MarsRoverTest.java
+++ src/Test/MarsRoverTest.java
@@ -1,20 +1,28 @@
 import org.junit.Test;
 
+import static javafx.scene.input.KeyCode.M;
 import static junit.framework.TestCase.assertEquals;
 
 
 public class MarsRoverTest {
+
     @Test
     public void shouldTurnWestFromNorthForInputL(){
         MarsRover marsRover= new MarsRover(0,0,Directions.NORTH);
-        marsRover.setDirections(Directions.Rotation.L);
+        marsRover.setDirections(Directions.Rotation.L,null);
         assertEquals(Directions.WEST,marsRover.getDirection());
     }
     @Test
     public void shouldTurnNorthFromWestForInputR(){
         MarsRover marsRover= new MarsRover(0, 0, Directions.WEST);
-        marsRover.setDirections(Directions.Rotation.R);
+        marsRover.setDirections(Directions.Rotation.R,null);
         assertEquals(Directions.NORTH,marsRover.getDirection());
+    }
+    @Test
+    public void shouldBeInTheSameDirectionForCommandM(){
+        MarsRover marsRover= new MarsRover(0, 0, Directions.WEST);
+        marsRover.setDirections(null,"M");
+        assertEquals(Directions.WEST,marsRover.getDirection());
     }
 
 
